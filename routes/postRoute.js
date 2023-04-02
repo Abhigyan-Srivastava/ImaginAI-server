e18097e3102ff504a5a7e9f5f0d3e22a69321d0a
Abhigyan-Srivastava <captainabhigyan.clasher@gmail.com>
2023-04-02 21:10:30 +0530

    Updated route

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -6,7 +6,7 @@ import {
 } from "../controllers/post.controller.js";
 const router = express.Router();
 
-router.route("/").get(getAllPosts);
+router.route("/allposts").get(getAllPosts);
 router.route("/").post(createPost);
 router.route("/:id").get(getSinglePost);
 
